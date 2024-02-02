c092eabec98a42a302ca641ba76338665cefc140
Pranav Goyal <pr945005@dal.ca>
2024-02-02 15:21:14 +0530

    Added code to consider Part Time employees

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,19 +9,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int status = rnd.Next(0, 2);
+            int status = rnd.Next(0, 3);
 
             int employeeWageRate = 20;
             int empHrs = 0;
 
-            if(status == 0)
+            if (status == 0)
             {
                 Console.WriteLine("Employee is Absent");
             }
+            else if (status == 1)
+            {
+                Console.WriteLine("Employee is Present and is Full-Time");
+                empHrs = 8;
+            }
             else
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                Console.WriteLine("Employee is Present and is Part-Time");
+                empHrs = 4;
             }
 
             int totalDailyWage = employeeWageRate * empHrs;
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33728199133827b58d8ab6adae7891b58f95bc2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92abbc354e4f3329fb8dfd61260c8a9081f8dab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
