b97e4f1614ad2fb3766d61b57777bc8fcc45be6e
Pranav Goyal <pr945005@dal.ca>
2024-02-02 16:15:19 +0530

    calculated monthly wage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,31 +9,42 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int status = rnd.Next(0, 3);
+            //int status = rnd.Next(0, 3);
 
             int employeeWageRate = 20;
             int empHrs = 0;
+            int numOfDays = 20;
+            int totalMonthlyWage = 0;
 
-            switch(status)
+            for(int day = 1; day<=numOfDays; day++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Absent");
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Present and is Full-Time");
-                    empHrs = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Present and is Part-Time");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Invalid input");
-                    break;
+                int status = rnd.Next(0, 3);
+                switch (status)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present and is Full-Time");
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Present and is Part-Time");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                }
+                Console.WriteLine("Day "+ day + " Wage : " + (employeeWageRate * empHrs));
+                totalMonthlyWage += (employeeWageRate * empHrs);
             }
             
-            int totalDailyWage = employeeWageRate * empHrs;
-            Console.WriteLine("Total Daily Wage: " + totalDailyWage);
+            
+            
+            Console.WriteLine("Total Monthly Wage: " + totalMonthlyWage);
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48399d3f2687151979ff59b9a828573db316404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f61e9ca0ef6eb1ff889e57b30b4f65c53a56f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
