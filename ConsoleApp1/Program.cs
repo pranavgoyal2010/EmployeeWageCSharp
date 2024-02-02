2f64aafdf1b48878cc0eb80ac8b70844b3b8a00a
Pranav Goyal <pr945005@dal.ca>
2024-02-02 14:55:07 +0530

    Displaying welcome message

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
+
